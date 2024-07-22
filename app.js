d9010c79580ff996c4bf03611920c95a010be3d9
himalf <himal.fullel15@gmail.com>
2024-07-22 22:18:49 +0545

    movie booking backend

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-routes.js";
+import adminRouter from "./routes/admin-routes.js";
+import movieRouter from "./routes/movie-routes.js";
+import bookingsRouter from "./routes/booking-routes.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+mongoose
+  .connect(
+    `mongodb+srv://himalsir:himal@cluster0.njjh2l6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connected To Database And Server is running")
+    )
+  )
+  .catch((e) => console.log(e));
